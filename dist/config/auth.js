985a086bac1ce3b473b937c80b418172a02223d1
Marcelin <marceloaugusto10123@gmail.com>
2023-10-14 03:03:53 -0300

    update

--- dist/config/auth.js
+++ dist/config/auth.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     secret: process.env.JWT_SECRET || "mysecret",
-    expiresIn: "15m",
+    expiresIn: "7d",
     refreshSecret: process.env.JWT_REFRESH_SECRET || "myanothersecret",
     refreshExpiresIn: "7d"
 };
